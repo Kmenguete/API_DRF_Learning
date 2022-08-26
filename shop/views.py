f45a59fbeac63430dcd4d57264b071cfd1bd01a1
Kmenguete <menguetekevin@gmail.com>
2022-08-26 10:03:21 +0200

    adding endpoint for products

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from shop.models import Category
-from shop.serializers import CategorySerializer
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryAPIView(APIView):
@@ -10,4 +10,12 @@ class CategoryAPIView(APIView):
     def get(self, *args, **kwargs):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+
+class ProductAPIView(APIView):
+
+    def get(self, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
